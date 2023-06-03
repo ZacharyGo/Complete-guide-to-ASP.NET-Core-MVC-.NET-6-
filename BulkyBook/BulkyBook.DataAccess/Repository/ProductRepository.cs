5b3b46ee000a96e7da8936cf2aa5bf34727a0e98
ZacharyGo <ZSGoEmpire@gmail.com>
2023-06-04 00:35:58 +0800

    .

---
+++ BulkyBook/BulkyBook.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,41 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDBContext _db;
+
+        public ProductRepository(ApplicationDBContext db) : base(db) 
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            var objFromDb = _db.Products.FirstOrDefault(p => p.Id== obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Title = obj.Title;
+                objFromDb.ISBN = obj.ISBN;
+                objFromDb.Price = obj;
+                objFromDb = obj;
+                objFromDb = obj;
+                objFromDb = obj;
+                objFromDb = obj;
+                objFromDb = obj;
+                objFromDb = obj;
+                objFromDb = obj;
+                objFromDb = obj;
+
+            }
+            _db.Products.Update(obj);
+        }
+    }
+}
