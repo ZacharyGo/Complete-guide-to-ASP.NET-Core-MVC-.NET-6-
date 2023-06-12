aeaa76a38dada6017d8e0f370713d9ce7a808ff2
Zachary-Fujitsu <zsgoempire@gmail.com>
2023-06-13 00:44:35 +0800

    With Register ad Login

---
+++ BulkyBook/BulkyBook.DataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,34 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        private ApplicationDBContext _db;
+        public CompanyRepository(ApplicationDBContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Company obj)
+        {
+            /*var objFromDb = _db.Companyies.FirstOrDefault(p => p.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = obj.Name;
+                objFromDb.StreetAddress = obj.StreetAddress;
+                objFromDb.City = obj.City;
+                objFromDb.State = obj.State;
+                objFromDb.PostalCode = obj.PostalCode;
+                objFromDb.PhoneNumber = obj.PhoneNumber;
+            }*/
+            _db.Companies.Update(obj);
+        }
+    }
+}
